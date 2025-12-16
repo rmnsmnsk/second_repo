d11dc7031cdcbc414b3071e30b62a548785695da
rmnsmnsk <136500209+rmnsmnsk@users.noreply.github.com>
2025-12-16 21:15:32 +0300

    Update merge.py

--- src/merge.py
+++ src/merge.py
@@ -1,35 +1,28 @@
-def merge(arr):
-
-    if len(arr) <= 1:
-        return arr
-
-    middle = len(arr)//2
-
-    left1 = arr[:middle]
-    right1 = arr[middle:]
-
-    left2 = merge(left1) #рекурсивно продолжаю делить половинки, пока не прихожу к ед массивам
-    right2 = merge(right1)
-
-    return merge_half(left2, right2)
-
 def merge_half(left, right):
-
     merged = []
-
-    while left and right:
-
-        el1 = left[0]
-        el2 = right[0]
-
-        if el1 > el2:
-            merged.append(right.pop(0))
+    i = 0
+    j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            merged.append(right[j])
+            j += 1
         else:
-            merged.append(left.pop(0))
-
-    merged.extend(left)
-    merged.extend(right) #либо оба пустые, либо один из низ
-
+            merged.append(left[i])
+            i += 1
+    
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    
     return merged
 
-
+def merge(arr):
+    if len(arr) <= 1:
+        return arr.copy()
+    
+    middle = len(arr) // 2
+    
+    left = merge(arr[:middle])
+    right = merge(arr[middle:])
+    
+    return merge_half(left, right)
