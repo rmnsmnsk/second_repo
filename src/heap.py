ffa337be093e5c9766fe8bc2bc669a467bca8b83
rmnsmnsk <136500209+rmnsmnsk@users.noreply.github.com>
2025-12-16 21:15:04 +0300

    Update heap.py

--- src/heap.py
+++ src/heap.py
@@ -1,54 +1,30 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+    
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
 def heap_sort(arr):
     if len(arr) == 0:
-        return arr
-
-    m = arr
+        return arr.copy()
+    
+    m = arr.copy()
     n = len(m)
-
-    for start in range(n//2 - 1, -1, -1):
-        current = start
-        while True:
-            data = m[current]
-            index = current
-            left = 2 * current + 1
-            right = 2 * current + 2
-
-            if left < n and m[left] > data:
-                index = left
-                data = m[left]
-
-            if right < n and m[right] > data:
-                index = right
-                data = m[right]
-
-            if index != current:
-                m[current], m[index] = m[index], m[current]
-                current = index
-            else:
-                break
-
-    for i in range(n-1, 0, -1):
+    
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(m, n, i)
+    
+    for i in range(n - 1, 0, -1):
         m[0], m[i] = m[i], m[0]
-        current = 0
-        while True:
-            data = m[current]
-            index = current
-            left = 2 * current + 1
-            right = 2 * current + 2
-
-            if left < i and m[left] > data:
-                index = left
-                data = m[left]
-
-            if right < i and m[right] > data:
-                index = right
-                data = m[right]
-
-            if index != current:
-                m[current], m[index] = m[index], m[current]
-                current = index
-            else:
-                break
-
+        heapify(m, i, 0)
+    
     return m
-
