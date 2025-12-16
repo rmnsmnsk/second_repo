3103c0971a3b4408c1287b10ff8b23f4c46597f0
rmnsmnsk <136500209+rmnsmnsk@users.noreply.github.com>
2025-12-16 21:16:17 +0300

    Update bubble.py

--- src/bubble.py
+++ src/bubble.py
@@ -1,12 +1,8 @@
-def bubble(m):
-    n = len(m)
-    flag = True
-    while flag:
-        flag = False
-        for j in range(n-1):
-            if m[j+1] < m[j]:
-                m[j], m[j+1] = m[j+1], m[j]
-                flag = True
-        if not flag:
-            break
-    return m
+def bubble(arr):
+    arr_copy = arr.copy()
+    n = len(arr_copy)
+    for i in range(n):
+        for j in range(n - 1):
+            if arr_copy[j] > arr_copy[j + 1]:
+                arr_copy[j], arr_copy[j + 1] = arr_copy[j + 1], arr_copy[j]
+    return arr_copy
